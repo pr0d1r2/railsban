ed2ff1337bb2aa02f001b0900d96287aadf88871
Marcin Nowicki <pr0d1r2@gmail.com>
2023-03-05 05:33:19 +0100

    Add missing route to make one test green

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :cards
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :home, only: :index
+
   # Defines the root path route ("/")
   root "home#index"
 end
