f47f8f776b41d478db3662f07feee8d2a36518f6
Marcin Nowicki <pr0d1r2@gmail.com>
2023-03-05 18:15:08 +0100

    Add statuses seed
    As we are unable to create card in development app.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Status.create!(name: 'Backlog')
+Status.create!(name: 'Refining')
+Status.create!(name: 'Ready')
+Status.create!(name: 'In Progress')
+Status.create!(name: 'UAT')
+Status.create!(name: 'Done')
+Status.create!(name: 'Cancelled')
