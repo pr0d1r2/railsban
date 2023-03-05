5165f8833930b710345fb0fc4a8f6f5f9eb88008
Marcin Nowicki <pr0d1r2@gmail.com>
2023-03-05 19:14:04 +0100

    Add missing spec coverage

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 require_relative '../../app/models/card'
 
 class CardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'delegates status name' do
+    assert cards(:one).status_name, statuses(:one).name
+  end
 end
