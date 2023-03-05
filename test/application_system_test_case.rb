0b4b32d8a997fcb52c2f0fc7f138d8b7911f035b
Marcin Nowicki <pr0d1r2@gmail.com>
2023-03-05 06:40:00 +0100

    Style/RequireOrder: Sort require in alphabetical order

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
-require 'webdrivers/chromedriver'
 require 'support/selenium_support'
+require 'webdrivers/chromedriver'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   selenium_support = ::SeleniumSupport.new
